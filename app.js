840441f847d85b646c38854bd3c9455d67089931
afifahrahmak <afifah.ark@gmail.com>
2022-07-19 09:47:54 +0700

    - done fetch product - include auth

--- app.js
+++ app.js
@@ -5,4 +5,13 @@ app.get('/register', (req, res) => {
     res.status(201).json({ message: 'success register' })
 })
 
+app.get('/products', (req, res) => {
+    let products = [
+        { id: 1, name: 'Laptop' },
+        { id: 2, name: 'Keyboard' }
+    ]
+    res.status(200).json(products)
+})
+
+
 app.listen(3000)
